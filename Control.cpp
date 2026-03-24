26e8cc88ca7788546af0199c5188e9489a13b860
Alejandro Perez <alejandroperezrdz@icloud.com>
2026-03-23 18:27:26 -0600

    Creacion de funciones y diseño de proAct segun diagrama de flujo

--- Control.cpp
+++ Control.cpp
@@ -10,7 +10,102 @@ const int MAX_ELEM = 50;
 const string FIN_ARCHIVO = "9999";
 
 //
+void reportarMovInvalido(){
+
+}
+
+//
+void insertRegistro(){
+
+}
+
+//
+void leerRegistroMov(){
+    
+}
+
+//
+void leerRegistroPer(){
+    
+}
+
+//
+void leerArchivoPer(){
+
+}
+
+//
+void generarReporte(){
+
+}
 
 //
 
+void procActualizacion(){
+    if (trab.M == trab.P){
+
+        switch (opt){
+        case "A":
+            procCopia();
+            reportarMovInvalido();
+            break;
+
+        case "B":
+            procBaja();
+            break;
+
+        case "C":
+            procCambio();
+            break;
+
+        default:
+            break;
+        }
+            
+    } else if (trab.M > trab.P){
+
+        switch (opt){
+        case "A":
+            procAlta();
+            reportarMovInvalido();
+            break;
+
+        case "B":
+            reportarMovInvalido();
+            leerRegistroMov();
+            break;
+
+        case "C":
+            reportarMovInvalido();
+            leerRegistroMov();
+            break;
+
+        default:
+            break;
+        }
+
+    } else {
+        reportarMovInvalido();
+    }
+    leerRegistroMov();
+    leerRegistroPer();
+}
+
 //
+void procBaja(){
+
+}
+//
+void procAlta(){
+
+}
+
+//
+void procCambio(){
+
+}
+
+//
+void procCopia(){
+
+}
